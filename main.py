6dfa05a8f029e51c21e02c4323dccdb6e21c3fd8
Lundaa <luoyunda@outlook.com>
2022-04-24 11:36:09 +0800

    revised format for maincode

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from my_function import Myfunction
+from my_function import MyFunction
 from config import ReadConfigFile
 from connect import Connect
 from list_helper import *
@@ -29,15 +29,15 @@ class MainProgram(object):
         """
         # 判断互联网chinaip来源连接
         if self.online_url == 1:
-            self.china_online_list = Myfunction().get_ispipipnet_list()
+            self.china_online_list = MyFunction().get_ispipipnet_list()
         elif self.online_url == 2:
-            self.china_online_list = Myfunction().get_ispclang_list()
+            self.china_online_list = MyFunction().get_ispclang_list()
         # 判断是否生成中国ip列表text文件
         if self.write_switch == 1:
-            Myfunction().write_text(self.china_online_list)
+            MyFunction().write_text(self.china_online_list)
         # 判断是否是自定义模式
         if self.custom_script_switch == 1:
-            Connect().ssh_coon(Myfunction().script_mode(self.script_file), self.time_out)
+            Connect().ssh_coon(MyFunction().script_mode(self.script_file), self.time_out)
         else:
             # 判断lock.ini是否存在
             if os.path.isfile("lock.ini"):
@@ -47,44 +47,44 @@ class MainProgram(object):
                     Connect().sftp_down_file(self.remote, self.local_path)
                     # 判断远程文件是否需要解压
                     if 'zip' in self.remote:
-                        Myfunction().un_zip(self.local_path + '/' + self.remote, self.local_path)
+                        MyFunction().un_zip(self.local_path + '/' + self.remote, self.local_path)
                     else:
                         pass
                     # 读取配置文件
-                    cfg_list = Myfunction().read_vrpcfg(self.local_cfg_path)
+                    cfg_list = MyFunction().read_vrpcfg(self.local_cfg_path)
                     # 遍历配置文件,获得fwdict字典
-                    fwdict = Myfunction().get_vrpcfg_chinaip_list(cfg_list)
+                    fwdict = MyFunction().get_vrpcfg_chinaip_list(cfg_list)
                     # 整形字典values值，获得fwlist列表
-                    fwlist = Myfunction().fw_alter_ip_list(fwdict)
+                    fwlist = MyFunction().fw_alter_ip_list(fwdict)
                     # 获取网络weblist
-                    weblist = Myfunction().web_alter_ip(self.china_online_list)
+                    weblist = MyFunction().web_alter_ip(self.china_online_list)
                     # 比较web中chinaip和fw配置文件中chinaip,获得删除和添加的ip_temp列表,返回值是一个元组类型
-                    change_temp = Myfunction().compare_list(weblist, fwlist)
+                    change_temp = MyFunction().compare_list(weblist, fwlist)
                     # 获得最终删除数据列表
-                    final_delete_list = Myfunction().get_delete_id(change_temp[1], fwdict)
+                    final_delete_list = MyFunction().get_delete_id(change_temp[1], fwdict)
                     # 获得最终添加数据列表
-                    final_add_list = Myfunction().get_add_list(change_temp[0])
+                    final_add_list = MyFunction().get_add_list(change_temp[0])
                     # 生成删除和添加列表的执行脚本
-                    scripts = Myfunction().update_ip_group(final_add_list, final_delete_list)
+                    scripts = MyFunction().update_ip_group(final_add_list, final_delete_list)
                     # 执行ssh连接，并传入脚本文件
                     Connect().ssh_coon(scripts, self.time_out)
                     # 输出脚本文件执行命令记录
-                    Myfunction().write_log(scripts)
+                    MyFunction().write_log(scripts)
                 except:
                     print("更新错误")
             else:
                 try:
                     print("first run!")
                     # 把网站上获取的数据进行整形处理
-                    weblist = Myfunction().web_alter_ip(self.china_online_list)
+                    weblist = MyFunction().web_alter_ip(self.china_online_list)
                     # 获取可插入华为防火墙ip组格式的中国ip列表,并均分为ip段数量为ip_number的列表,返回的是一个生成器
                     china_ip_generator = ListHelper.list_cut(weblist, self.ip_number)
                     # 生成第一次运行本程序时的脚本文件
-                    scripts = Myfunction().first_huawei_scripts(china_ip_generator)
+                    scripts = MyFunction().first_huawei_scripts(china_ip_generator)
                     # 连接ssh并写入数据，成功运行后会输出一个lock.ini锁定文件,第一次运行时间较长，大约30分钟
                     Connect().ssh_coon(scripts, self.time_out)
                     # 输出脚本文件执行命令记录,默认不开启，第一次运行时的脚本命令过多，可能影响效能，调试时可开启
-                    # Myfunction().write_script_log(scripts)
+                    MyFunction().write_log(scripts)
                 except:
                     print("运行错误！")
 
